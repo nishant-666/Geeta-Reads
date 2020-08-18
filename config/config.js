9e4f7262efb81575eea4071d5a21a4158e1c3684
Nishant Kumar <33790075+nishant-666@users.noreply.github.com>
2020-08-18 10:19:14 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyAQa206OBSe3XYGKFLNc5nCWKwYy9iMA5M",
-  authDomain: "upheld-dragon-275605.firebaseapp.com",
-  databaseURL: "https://upheld-dragon-275605.firebaseio.com",
-  projectId: "upheld-dragon-275605",
-  storageBucket: "upheld-dragon-275605.appspot.com",
-  messagingSenderId: "785936912745",
-  appId: "1:785936912745:web:e4985248abf49a31792df8",
-  measurementId: "G-TNSMZVM6E9"
+ //Paste your config credentials here.
   };
 
   firebase.initializeApp(config)
